ee1827174f81b3b5cab14f8ffb09ac57309e7d17
Bwunkaz <122973984+Bwunkaz@users.noreply.github.com>
2024-02-27 20:23:40 +0100

    done some more animations

--- src/components/Scene/Model.js
+++ src/components/Scene/Model.js
@@ -1,53 +1,107 @@
-import React, { useRef } from "react";
-import { MeshTransmissionMaterial, useGLTF, Text } from "@react-three/drei";
+import React, { useRef, useState, useEffect } from "react";
+import {
+  MeshTransmissionMaterial,
+  useGLTF,
+  Text as DreiText,
+} from "@react-three/drei";
 import { useFrame, useThree } from "@react-three/fiber";
+import { useSpring, animated } from "@react-spring/three";
 
-// import { useControls } from "leva";
+const AnimatedMeshTransmissionMaterial = animated(MeshTransmissionMaterial);
+const AnimatedText = animated(DreiText);
+
+function useWindowMouseMove() {
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+  useEffect(() => {
+    function handleMouseMove(event) {
+      setMousePosition({ x: event.clientX, y: event.clientY });
+    }
+    window.addEventListener("mousemove", handleMouseMove);
+    return () => window.removeEventListener("mousemove", handleMouseMove);
+  }, []);
+  return mousePosition;
+}
+
+function calculateDistanceFromCenter(x, y) {
+  return Math.sqrt(
+    Math.pow(x - window.innerWidth / 2, 2) +
+      Math.pow(y - window.innerHeight / 2, 2)
+  );
+}
 
 export default function Model() {
   const { nodes } = useGLTF("/medias/torrus.glb");
   const { viewport } = useThree();
-  const torus = useRef(null);
+  const circleLoaded = useRef();
+
+  const { x, y } = useWindowMouseMove();
+  const distanceFromCenter = calculateDistanceFromCenter(x, y);
+  const maxDistance = calculateDistanceFromCenter(
+    window.innerWidth,
+    window.innerHeight
+  );
+  const isCloseToCenter = distanceFromCenter < window.innerWidth / 4;
+  const scaleValue = isCloseToCenter ? [1, 1, 1] : [0.8, 0.8, 0.8];
 
   useFrame(() => {
-    torus.current.rotation.x += 0.015;
+    if (circleLoaded.current) {
+      circleLoaded.current.rotation.x += 0.015;
+    }
   });
 
-  const materialProps = {
-    thickness: 0,
-    roughness: 0,
+  const circlePosition = useSpring({
+    position: isCloseToCenter ? [-1.2, -0.01, 0] : [0, 0, 0],
+    config: { mass: 5, tension: 300, friction: 50 },
+  });
+
+  const startingMeshValues = useSpring({
+    scale: scaleValue,
+    color: "white",
+    thickness: 1,
+    roughness: isCloseToCenter ? 2 : 0,
     transmission: 1,
     ior: 0.9,
     chromaticAberration: 1,
     backside: true,
-    color: "#FF9D18",
-  };
+    config: { tension: 100, friction: 20 },
+  });
 
-  // const materialProps = {
-  //   thickness: 0.2,
-  //   roughness: 0,
-  //   transmission: 1,
-  //   ior: 1.2,
-  //   chromaticAberration: 0.02,
-  //   backside: true,
-  // };
+  const dynamicMeshValues = useSpring({
+    scale: isCloseToCenter ? [0.4, 0.4, 0.4] : [1, 1, 1],
+    config: { tension: 70, friction: 7 },
+  });
+
+  const letterSpacingSpring = useSpring({
+    letterSpacing: isCloseToCenter ? 0.5 : 0.2,
+    config: { tension: 70, friction: 20 },
+  });
+
+  const lettersStyling = {
+    font: "/fonts/PPNeueMontreal-Bold.otf",
+    fontSize: 0.5,
+    anchorX: "center",
+    anchorY: "middle",
+    letterSpacing: letterSpacingSpring.letterSpacing,
+    opacity: 0,
+    // visible: isCloseToCenter ? true : false,
+    color: isCloseToCenter ? "black" : "white",
+    scale: scaleValue,
+  };
 
   return (
     <group scale={viewport.width / 3.75}>
-      <Text
-        font={"/fonts/PPNeueMontreal-Bold.otf"}
-        position={[0, 0, -1]}
-        fontSize={0.5}
-        color="white"
-        anchorX="center"
-        anchorY="middle"
-        letterSpacing={0}
+      <animated.mesh
+        ref={circleLoaded}
+        {...nodes.Torus002}
+        position={circlePosition.position}
+        {...startingMeshValues}
+        {...dynamicMeshValues}
       >
-        Oskar herman
-      </Text>
-      <mesh ref={torus} {...nodes.Torus002}>
-        <MeshTransmissionMaterial {...materialProps} />
-      </mesh>
+        <AnimatedMeshTransmissionMaterial {...startingMeshValues} />
+      </animated.mesh>
+      <AnimatedText {...lettersStyling}>
+        {isCloseToCenter ? `  SKAR` : "Herman"}
+      </AnimatedText>
     </group>
   );
 }
--- src/components/Scene/index.jsx
+++ src/components/Scene/index.jsx
@@ -3,9 +3,11 @@ import { Canvas } from "@react-three/fiber";
 import Model from "./Model";
 import { Environment } from "@react-three/drei";
 
+// bg-[#FDF9EF]
+
 export default function Index() {
   return (
-    <Canvas className="bg-[#FDF9EF]">
+    <Canvas className="bg-[#E43B13]">
       <Model />
       <directionalLight intensity={2} position={[0, 2, 3]} />
       <Environment preset="city" />
