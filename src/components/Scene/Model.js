ba9d1d707d021edaba1eac7cbdedfa652c292580
Bwunkaz <122973984+Bwunkaz@users.noreply.github.com>
2024-02-27 21:46:06 +0100

    .

--- src/components/Scene/Model.js
+++ src/components/Scene/Model.js
@@ -6,6 +6,7 @@ import {
 } from "@react-three/drei";
 import { useFrame, useThree } from "@react-three/fiber";
 import { useSpring, animated } from "@react-spring/three";
+import { debounce } from "lodash";
 
 const AnimatedMeshTransmissionMaterial = animated(MeshTransmissionMaterial);
 const AnimatedText = animated(DreiText);
@@ -22,39 +23,29 @@ function useWindowMouseMove() {
   return mousePosition;
 }
 
-function calculateDistanceFromCenter(x, y) {
-  return Math.sqrt(
-    Math.pow(x - window.innerWidth / 2, 2) +
-      Math.pow(y - window.innerHeight / 2, 2)
-  );
-}
-
 export default function Model() {
   const { nodes } = useGLTF("/medias/torrus.glb");
   const { viewport } = useThree();
   const circleLoaded = useRef();
 
-  const { x, y } = useWindowMouseMove();
-  const distanceFromCenter = calculateDistanceFromCenter(x, y);
-  const isCloseToCenter = distanceFromCenter < window.innerWidth / 8;
-  const scaleValue = isCloseToCenter ? [1, 1, 1] : [0.8, 0.8, 0.8];
+  const [isHovered, setIsHovered] = useState(false);
 
   useFrame(() => {
     if (circleLoaded.current) {
-      circleLoaded.current.rotation.x += 0.015;
+      circleLoaded.current.rotation.x += isHovered ? 0.06 : 0.015;
     }
   });
 
   const circlePosition = useSpring({
-    position: isCloseToCenter ? [-1.2, -0.01, 0] : [0, 0, 0],
+    position: isHovered ? [-1.2, -0.01, 0] : [0, 0, 0],
     config: { mass: 5, tension: 300, friction: 50 },
   });
 
   const startingMeshValues = useSpring({
-    scale: scaleValue,
+    scale: isHovered ? [1, 1, 1] : [0.8, 0.8, 0.8],
     color: "white",
     thickness: 1,
-    roughness: isCloseToCenter ? 2 : 0,
+    roughness: isHovered ? 2 : 0,
     transmission: 1,
     ior: 0.9,
     chromaticAberration: 1,
@@ -63,14 +54,16 @@ export default function Model() {
   });
 
   const dynamicMeshValues = useSpring({
-    scale: isCloseToCenter ? [0.4, 0.4, 0.4] : [1, 1, 1],
+    scale: isHovered ? [0.4, 0.4, 0.4] : [1, 1, 1],
     config: { tension: 70, friction: 7 },
   });
 
   const letterSpacingSpring = useSpring({
-    letterSpacing: isCloseToCenter ? 0.5 : 0.2,
+    letterSpacing: isHovered ? 0.5 : 0.2,
     config: { tension: 70, friction: 20 },
   });
+
+  const debouncedSetIsHovered = debounce(setIsHovered, 25);
 
   const lettersStyling = {
     font: "/fonts/PPNeueMontreal-Bold.otf",
@@ -79,9 +72,10 @@ export default function Model() {
     anchorY: "middle",
     letterSpacing: letterSpacingSpring.letterSpacing,
     opacity: 0,
-    // visible: isCloseToCenter ? true : false,
-    color: isCloseToCenter ? "black" : "white",
-    scale: scaleValue,
+    color: isHovered ? "black" : "white",
+    scale: isHovered ? [1, 1, 1] : [0.8, 0.8, 0.8],
+    onPointerOver: () => debouncedSetIsHovered(true),
+    onPointerOut: () => debouncedSetIsHovered(false),
   };
 
   return (
@@ -96,7 +90,7 @@ export default function Model() {
         <AnimatedMeshTransmissionMaterial {...startingMeshValues} />
       </animated.mesh>
       <AnimatedText {...lettersStyling}>
-        {isCloseToCenter ? `  SKAR` : "Herman"}
+        {isHovered ? `  SKAR` : "Herman"}
       </AnimatedText>
     </group>
   );
