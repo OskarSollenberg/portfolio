8bd4d1235b91471c79b3d9264b263f48860886f0
Bwunkaz <122973984+Bwunkaz@users.noreply.github.com>
2024-02-29 02:28:34 +0100

    .

--- src/components/Scene/Model.js
+++ src/components/Scene/Model.js
@@ -1,16 +1,13 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef, useState, useEffect, use } from "react";
 import { useGLTF, Text as DreiText } from "@react-three/drei";
 import { useThree, useFrame } from "@react-three/fiber";
 import { MeshTransmissionMaterial } from "@react-three/drei";
 import { gsap } from "gsap";
 import { useSpring, animated } from "react-spring";
 
-import { Vector3, BufferGeometry, LineBasicMaterial } from "three";
-
-const AnimatedText = animated(DreiText);
-const AnimatedMesh = animated(MeshTransmissionMaterial);
-
 export default function Model() {
+  const AnimatedText = animated(DreiText);
+  const AnimatedMesh = animated(MeshTransmissionMaterial);
   const meshRef = useRef();
   const textRef = useRef();
   const { viewport } = useThree();
@@ -18,22 +15,43 @@ export default function Model() {
 
   const [isHovered, setIsHovered] = useState(false);
 
-  const startingMeshValues = useSpring({
-    scale: isHovered ? [1, 1, 1] : [0.8, 0.8, 0.8],
-    color: "#F97315",
-    thickness: 0.2,
-    roughness: isHovered ? 2 : 0,
-    transmission: 1,
-    ior: 1,
-    chromaticAberration: 1,
-    backside: true,
+  useEffect(() => {
+    setIsHovered(true);
+    const timeoutId = setTimeout(() => {
+      setIsHovered(false);
+    }, 900);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, []);
+
+  const [initialStyling, setInitialStyling] = useState(true);
+  const [afterStyling, setAfterStyling] = useState(false);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setInitialStyling(false);
+      setAfterStyling(true);
+    }, 1800);
+
+    return () => clearTimeout(timeoutId);
+  }, []);
+
+  // maby use white
+  const meshValues = useSpring({
+    color: initialStyling ? "white" : afterStyling ? "#F97315" : "black",
+    thickness: initialStyling ? 4 : afterStyling ? 0.2 : 0.1,
+    roughness: initialStyling ? 0 : afterStyling ? (isHovered ? 2 : 0) : 0.5,
+    transmission: initialStyling ? 1 : afterStyling ? 1 : 0.5,
+    ior: initialStyling ? 2 : afterStyling ? 1 : 0.5,
+    chromaticAberration: initialStyling ? 0 : afterStyling ? 1 : 0.5,
+    backside: initialStyling ? false : afterStyling ? true : false,
     config: { tension: 80, friction: 10 },
   });
 
   useFrame(() => {
-    if (meshRef.current) {
-      meshRef.current.rotation.x += isHovered ? 0.05 : 0.015;
-    }
+    meshRef.current.rotation.x += isHovered ? 0.05 : 0.015;
   });
 
   // Hover
@@ -42,60 +60,67 @@ export default function Model() {
     clearTimeout(hoverTimeout);
     hoverTimeout = setTimeout(() => {
       setIsHovered(newHoverState);
-    }, 40); // Increase delay to 100ms
+    }, 40);
   };
+
   useEffect(() => {
-    gsap.to(meshRef.current.position, {
-      x: isHovered ? -1.2 : 0,
-      duration: 1.5,
-      ease: "elastic.out(0.1, 0.1)",
-    });
-    gsap.to(meshRef.current.scale, {
-      x: isHovered ? 0.4 : 1,
-      y: isHovered ? 0.4 : 1,
-      z: isHovered ? 0.4 : 1,
-      duration: 1.5,
-      ease: "elastic.out(0.1, 0.1)",
-    });
+    if (meshRef.current) {
+      gsap.to(meshRef.current.position, {
+        x: isHovered ? -1.2 : 0,
+        duration: 1.5,
+        ease: "elastic.out(0.1, 0.1)",
+      });
+      gsap.to(meshRef.current.scale, {
+        x: isHovered ? 0.4 : 1,
+        y: isHovered ? 0.4 : 1,
+        z: isHovered ? 0.4 : 1,
+        duration: 1.5,
+        ease: "elastic.out(0.1, 0.1)",
+      });
+    }
   }, [isHovered]);
 
   // Letters
   const letterSpacingSpring = useSpring({
-    letterSpacing: isHovered ? 0.5 : 0.2,
+    letterSpacing: isHovered ? 0.5 : initialStyling ? 0.8 : 0.2,
     config: { tension: 100, friction: 10 },
   });
   const lettersStyling = {
     font: "/fonts/PPNeueMontreal-Bold.otf",
     fontSize: 0.5,
-    anchorX: "center",
-    anchorY: "middle",
     letterSpacing: letterSpacingSpring.letterSpacing,
-    color: isHovered ? "black" : "#FDF9EF",
+    color: initialStyling
+      ? isHovered
+        ? "yellow"
+        : "purple"
+      : afterStyling
+      ? isHovered
+        ? "black"
+        : "white"
+      : "black",
     scale: isHovered ? [1, 1, 1] : [0.8, 0.8, 0.8],
     onPointerOver: () => handleHover(true),
     onPointerOut: () => handleHover(false),
+    isVisible: true,
   };
-
-  function Line({ start, end }) {
-    const points = [start, end];
-    const geometry = new BufferGeometry().setFromPoints(points);
-    const material = new LineBasicMaterial({ color: "black" });
-    return <line geometry={geometry} material={material} />;
-  }
 
   return (
     <group scale={viewport.width / 3.75}>
       <mesh ref={meshRef} geometry={nodes.Torus002.geometry}>
-        <AnimatedMesh {...startingMeshValues} />
+        <AnimatedMesh {...meshValues} />
       </mesh>
       <AnimatedText
         ref={textRef}
         style={letterSpacingSpring}
         {...lettersStyling}
+        visible={lettersStyling.isVisible}
       >
-        {isHovered ? "  SKAR" : "Herman"}
+        {isHovered
+          ? ["  ", "S", "K", "A", "R"]
+          : initialStyling
+          ? "HERMAN"
+          : "Herman"}
       </AnimatedText>
-      <Line start={new Vector3(-0.5, 0, 0.1)} end={new Vector3(0.5, 0, 0.1)} />
     </group>
   );
 }
