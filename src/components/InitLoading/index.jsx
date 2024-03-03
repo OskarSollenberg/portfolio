be7c4d3617271cb98a5359ab1fcb27df55bbb195
Bwunkaz <122973984+Bwunkaz@users.noreply.github.com>
2024-03-03 09:31:11 +0100

    .

--- src/components/InitLoading/index.jsx
+++ src/components/InitLoading/index.jsx
@@ -1,38 +1,52 @@
 "use client";
-import React, { useEffect } from "react";
+
+import React, { useEffect, useRef } from "react";
+import { gsap } from "gsap";
 import "./styles.css";
 import useLoading from "@/src/hooks/useLoading";
 
 export default function InitLoading() {
   const loading = useLoading();
+  const circleRef = useRef(null);
+  const divRef = useRef(null);
 
-  //   useEffect(() => {
-  //     document.body.style.overflow = loading ? "hidden" : "auto";
+  useEffect(() => {
+    if (!loading) {
+      gsap.to(circleRef.current, {
+        duration: 1,
+        opacity: 0,
+        ease: "power1.in",
+      });
 
-  //     return () => {
-  //       document.body.style.overflow = "auto";
-  //     };
-  //   }, [loading]);
+      gsap.to(divRef.current, {
+        duration: 1,
+        opacity: 0,
+        ease: "power1.in",
+      });
+    }
+  }, [loading]);
 
   return (
     <div
-      className={`absolute w-screen h-screen transition-opacity duration-1000 z-1 bg-[black] ${
-        loading ? "opacity-100" : "opacity-0 pointer-events-none"
-      }`}
+      ref={divRef}
+      className="absolute w-screen h-screen z-1 bg-[black]"
+      style={{ opacity: 1 }}
     >
       <div className="flex justify-center items-center h-screen">
         <svg
-          className={`loader z-50 ${loading ? "" : "hidden"}`}
+          className="loader z-50"
           viewBox="0 0 50 50"
           style={{ mixBlendMode: "difference" }}
         >
           <circle
+            ref={circleRef}
             className="path"
             cx="25"
             cy="25"
             r="20"
             fill="none"
             strokeWidth="0.2"
+            style={{ opacity: 1 }}
           ></circle>
         </svg>
       </div>
