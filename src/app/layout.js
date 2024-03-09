0cc01f09540c1e12cbac91f5e90594306c595168
Bwunkaz <122973984+Bwunkaz@users.noreply.github.com>
2024-03-09 14:52:02 +0100

    .

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={` ${inter.className}`}>
-        <div className="flex relative justify-center items-center">
+        <div className="flex justify-center items-center">
           {children}
           <MainNav index={200} />
           <ProgressBarComp index={200} />
