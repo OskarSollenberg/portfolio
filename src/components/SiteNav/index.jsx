405a937cc5833f141b1ebcc4f5857a2023a18428
Bwunkaz <122973984+Bwunkaz@users.noreply.github.com>
2024-03-20 17:45:56 +0100

    .

--- src/components/SiteNav/index.jsx
+++ src/components/SiteNav/index.jsx
@@ -20,7 +20,7 @@ export default function SiteNav({ isHomepage }) {
         style={{
           fontFamily: "Montreal",
         }}
-        className={` fixed flex items-center rounded-lg justify-start gap-[1rem] -translate-x-[50%] md:translate-x-0 left-[50%]  md:left-[90%]  bottom-[2rem] text-[#FDF9EF] text-sm transition-all duration-300 ease-in-out z-[4000] p-[6px]
+        className={` fixed flex items-center rounded-lg justify-start gap-[1rem] -translate-x-[50%] md:translate-x-0 left-[50%]  md:left-[4rem] bottom-[2rem] text-[#FDF9EF] text-sm transition-all duration-300 ease-in-out z-[4000] p-[6px]
 		${isActive ? "w-[4rem] " : "w-[4rem] md:w-auto"}
 		`}
       >
