0e51413a4e91848f1abe6401baa5788799ebf18d
Bwunkaz <122973984+Bwunkaz@users.noreply.github.com>
2024-03-30 23:38:13 +0100

    .

--- src/app/projects/[project]/page.jsx
+++ src/app/projects/[project]/page.jsx
@@ -36,16 +36,15 @@ export default function Project() {
   } = selectedProject;
 
   return (
-    <section className="block w-screen h-screen lg:flex justify-center items-center ">
+    <section className="">
       <div className="absolute z-[500] top-[2rem] left-[2rem] text-[1rem] hover:bg-[#F3691F] rounded-lg  transition-all hover:scale-125 duration-200 bg-black text-white hover:text-black cursor-pointer p-2 ">
         <Link href="/projects">
           <RiArrowGoBackFill />
         </Link>
       </div>
-
-      <div className="flex mt-[4rem] flex-col-reverse lg:grid items-center justify-center lg:grid-cols-2 text-center lg:text-start">
-        <div className="w-full h-full text-black flex justify-center items-center md:px-10 sm:mt-0 md:mt-0">
-          <div className=" flex flex-col gap-[2.5rem]">
+      <div className="flex  flex-col-reverse lg:grid items-center justify-center lg:grid-cols-2 text-center lg:text-start">
+        <div className="w-full h-screen text-black flex justify-center item-center lg:items-end md:px-10 sm:mt-0 md:mt-0 bg-[#F3691F]">
+          <div className=" flex flex-col mt-[3rem] lg:mt-0 lg:justify-end gap-[2rem]">
             <div className="px-[1rem]">
               <h2 className="text-3xl font-bold md:text-4xl lg:text-5xl">
                 {title}
@@ -54,45 +53,45 @@ export default function Project() {
                 {description}
               </p>
             </div>
-            <div className="flex flex-col gap-4 px-[1rem] h-full">
+            <div className="flex flex-col gap-4 px-[1rem]">
               <h2 className="text-2xl font-bold md:text-2xl">
                 Biggest learnings
               </h2>
-              <ul className="flex flex-col gap-4 lg:gap-1 text-[.8rem] lg:text-[.9rem] italic translate-x-0">
+              <ul className="flex flex-col gap-2 lg:gap-[0.6rem] text-[.8rem] lg:text-[.9rem] italic">
                 {learnings.map((project, index) => (
                   <li key={index}> - {project.learning}</li>
                 ))}
               </ul>
-            </div>
-            <div className="flex justify-center gap-[8rem] mb-[3rem] mt-[2rem] ">
-              <a
-                href={github}
-                className="flex group flex-col gap-4 justify-center items-center ml-[3rem] sm:ml-0"
-              >
-                <StickyLink
-                  color={"#000"}
-                  icon={<FaGithub />}
-                  size={50}
-                  type={"github"}
-                />
+              <div className="flex justify-center gap-[8rem] mb-[3rem] mt-[2rem] ">
+                <a
+                  href={github}
+                  className="flex group flex-col gap-4 justify-center items-center ml-[3rem] sm:ml-0"
+                >
+                  <StickyLink
+                    color={"#000"}
+                    icon={<FaGithub />}
+                    size={50}
+                    type={"github"}
+                  />
 
-                <p className="group-hover:opacity-0 text-[12px]">VIEW CODE</p>
-              </a>
-              <a
-                href={netlify}
-                className="flex group flex-col gap-4 justify-center items-center mr-[3rem] sm:mr-0"
-              >
-                <StickyLink
-                  color={"#000"}
-                  icon={<BiLogoNetlify />}
-                  size={50}
-                  type={"netlify"}
-                />
+                  <p className="group-hover:opacity-0 text-[12px]">VIEW CODE</p>
+                </a>
+                <a
+                  href={netlify}
+                  className="flex group flex-col gap-4 justify-center items-center mr-[3rem] sm:mr-0"
+                >
+                  <StickyLink
+                    color={"#000"}
+                    icon={<BiLogoNetlify />}
+                    size={50}
+                    type={"netlify"}
+                  />
 
-                <p className="group-hover:opacity-0 text-[12px]">
-                  LAUNCH WEBSITE
-                </p>
-              </a>
+                  <p className="group-hover:opacity-0 text-[12px]">
+                    LAUNCH WEBSITE
+                  </p>
+                </a>
+              </div>
             </div>
           </div>
         </div>
