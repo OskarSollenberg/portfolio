a058be3a07a0e1a308e5b1f75c0452ab53eae36b
Bwunkaz <122973984+Bwunkaz@users.noreply.github.com>
2024-04-02 14:09:03 +0200

    ipad landscape

--- src/components/SceneHome/index.jsx
+++ src/components/SceneHome/index.jsx
@@ -3,21 +3,41 @@ import { Canvas } from "@react-three/fiber";
 import Model from "./Model";
 import { Environment } from "@react-three/drei";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function SceneHome() {
+  const [height, setHeight] = useState(null);
+
   useEffect(() => {
     const originalStyle = window.getComputedStyle(document.body).cssText;
-    document.body.style.cssText = "position: fixed; top: -${window.scrollY}px";
+    document.body.style.cssText = `position: fixed; top: -${window.scrollY}px`;
+
+    // Set the height state to the window's inner height
+    setHeight(window.innerHeight);
+
+    // Update the height state whenever the window is resized
+    const handleResize = () => {
+      setHeight(window.innerHeight);
+    };
+
+    window.addEventListener("resize", handleResize);
 
     return () => {
       document.body.style.cssText = originalStyle;
       window.scrollTo(0, parseInt(document.body.style.top || "0") * -1);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
 
   return (
-    <div className={`w-screen h-screen -z-10`}>
+    <div
+      style={{
+        width: "100vw",
+        height: `${height}px`,
+        position: "absolute",
+        zIndex: -10,
+      }}
+    >
       <Canvas>
         <Model />
         <directionalLight />
