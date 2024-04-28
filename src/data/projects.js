13d9424fddacf0a0088ea83ed655e1a4983d3ebf
Bwunkaz <122973984+Bwunkaz@users.noreply.github.com>
2024-04-28 18:58:18 +0200

    .

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,147 +1,183 @@
 export const projects = [
   {
-    title: "Hinder",
-    href: "hinder",
+    title: 'Mentatt',
+    href: 'Mentatt',
+    subTitle: 'Teamwork Makes The Dreamwork',
+    imgSrc: '/medias/mentatt.webp',
+    videoWebm: '/medias/mentatt.webm',
+    videoMp4: '/medias/mentatt.mp4',
+    github: '#',
+    netlify: 'https://vercel.com/mentatt/mentatt',
+    description:
+      'This is a collaboration project done together with UX-designer at Hyper Island where we got to build a landing page for the mental wellness company Mentatt. The website is built using Next.js, well documented using Storybook and have integrated language translation using the i18 nexus library.',
+    learnings: [
+      {
+        learning: 'Working with Agile methodologies',
+      },
+      { learning: 'Building website from Wireframe/Prototype' },
+      {
+        learning: 'Creating improved Versions based of user Feedback',
+      },
+      {
+        learning: 'Using Reusable components / reusable UI components,',
+      },
+      {
+        learning: 'Finding and creating commonalities in design prototype',
+      },
+      {
+        learning: 'Storybook for component library',
+      },
+      {
+        learning: 'Jest for testing',
+      },
+      {
+        learning: 'i18 nexus for translations',
+      },
+    ],
+  },
+  {
+    title: 'Hinder',
+    href: 'hinder',
     subTitle: "It's a dating app",
-    imgSrc: "/medias/hinder.webp",
-    videoWebm: "/medias/hinder.webm",
-    videoMp4: "/medias/hinder.mp4",
-    github: "https://github.com/OskarSollenberg/Hinder",
-    netlify: "https://portfolio-oskar-new--hinderr.netlify.app",
+    imgSrc: '/medias/hinder.webp',
+    videoWebm: '/medias/hinder.webm',
+    videoMp4: '/medias/hinder.mp4',
+    github: 'https://github.com/OskarSollenberg/Hinder',
+    netlify: 'https://portfolio-oskar-new--hinderr.netlify.app',
     description:
-      "A dating app that enables you to set up a profile, browse through other profiles by swiping, and match with others. This app features account creation, along with the flexibility to log out and log back in, utilizing local storage to mimic the functionality of a database.",
+      'A dating app that enables you to set up a profile, browse through other profiles by swiping, and match with others. This app features account creation, along with the flexibility to log out and log back in, utilizing local storage to mimic the functionality of a database.',
     learnings: [
       {
         learning:
-          "How to create a React application with dynamic, interactive user interfaces.",
+          'How to create a React application with dynamic, interactive user interfaces.',
       },
       {
-        learning: "Understanding component state, props, and hooks.",
+        learning: 'Understanding component state, props, and hooks.',
       },
       {
-        learning: "Learning more about fetching data using APIs.",
+        learning: 'Learning more about fetching data using APIs.',
       },
       {
-        learning: "Understanding how to set up and use context.",
+        learning: 'Understanding how to set up and use context.',
       },
       {
         learning:
-          "Client-side registration, authorization, and authentication.",
+          'Client-side registration, authorization, and authentication.',
       },
       {
-        learning: "Learning how to use local storage.",
+        learning: 'Learning how to use local storage.',
       },
     ],
   },
   {
-    title: "PetFinder",
-    href: "petfinder",
-    subTitle: "Its all about the SEO and Performance!!",
-    imgSrc: "/medias/petfinder.webp",
-    videoWebm: "/medias/petfinder.webm",
-    videoMp4: "/medias/petfinder.mp4",
-    github: "https://github.com/OskarSollenberg/petfinder",
-    netlify: "https://petfinder-group4.netlify.app/",
+    title: 'PetFinder',
+    href: 'petfinder',
+    subTitle: 'Its all about the SEO and Performance!!',
+    imgSrc: '/medias/petfinder.webp',
+    videoWebm: '/medias/petfinder.webm',
+    videoMp4: '/medias/petfinder.mp4',
+    github: 'https://github.com/OskarSollenberg/petfinder',
+    netlify: 'https://petfinder-group4.netlify.app/',
     description:
-      "PetFinder is a project focusing on SEO, performance, and accessibility. This was about a 2-week project that me and three other students created during one of our modules at Hyper Island.",
+      'PetFinder is a project focusing on SEO, performance, and accessibility. This was about a 2-week project that me and three other students created during one of our modules at Hyper Island.',
     learnings: [
       {
-        learning: "Understanding and reasoning around performance.",
+        learning: 'Understanding and reasoning around performance.',
       },
-      { learning: "Analyzing Lighthouse metrics." },
+      { learning: 'Analyzing Lighthouse metrics.' },
       {
-        learning: "How to optimize a website for conversion.",
+        learning: 'How to optimize a website for conversion.',
       },
       {
         learning:
-          "Learning how to achieve a fast website through coding best practices.",
+          'Learning how to achieve a fast website through coding best practices.',
       },
       {
-        learning: "Making a website that is accessible for all.",
+        learning: 'Making a website that is accessible for all.',
       },
       {
-        learning: "SEO practice for optimizing websites.",
-      },
-    ],
-  },
-
-  {
-    title: "PencilPaws",
-    href: "pencilpaws",
-    subTitle: "Use the apple pencil on an ipad!",
-    imgSrc: "/medias/pencil-paws.webp",
-    videoWebm: "/medias/pencilpaws.webm",
-    videoMp4: "/medias/pencilpaws.mp4",
-    github: "https://github.com/OskarSollenberg/PencilPaws",
-    netlify: "https://pencilpawsipad.netlify.app",
-    description:
-      "A 2-player game where you guess the animal your friend is drawing. The game is played in real time, and you can use the Apple Pencil on an iPad to draw.",
-    learnings: [
-      {
-        learning:
-          "JavaScript concepts such as variables, loops, functions, objects, and arrays.",
-      },
-      { learning: "DOM API to query, modify, and create elements." },
-      {
-        learning: "Using event listeners to add interactivity.",
-      },
-      { learning: "Using the Web Storage API to persist data." },
-      {
-        learning: "Fetch API to get data from a third party.",
-      },
-      {
-        learning: "Working with canvas and making it work on all devices.",
+        learning: 'SEO practice for optimizing websites.',
       },
     ],
   },
 
   {
-    title: "Nordstan",
-    href: "nordstan",
-    subTitle: "The most used bus in Gothenburg is bus number 16",
-    imgSrc: "/medias/nordstan.webp",
-    videoWebm: "/medias/nordstan.webm",
-    videoMp4: "/medias/nordstan.mp4",
-    github: "https://github.com/OskarSollenberg/ITTT---ResRobot_Nordstan",
-    netlify: "#",
+    title: 'PencilPaws',
+    href: 'pencilpaws',
+    subTitle: 'Use the apple pencil on an ipad!',
+    imgSrc: '/medias/pencil-paws.webp',
+    videoWebm: '/medias/pencilpaws.webm',
+    videoMp4: '/medias/pencilpaws.mp4',
+    github: 'https://github.com/OskarSollenberg/PencilPaws',
+    netlify: 'https://pencilpawsipad.netlify.app',
     description:
-      "This is a website I created in an early module at Hyper Island. Using an animation I created, the website displays in real-time when bus 16 arrives and leaves Nordstan in Gothenburg. The website is made with HTML, CSS/SASS, and JavaScript.",
+      'A 2-player game where you guess the animal your friend is drawing. The game is played in real time, and you can use the Apple Pencil on an iPad to draw.',
     learnings: [
       {
         learning:
-          "Learning JavaScript concepts such as variables, loops, functions.",
+          'JavaScript concepts such as variables, loops, functions, objects, and arrays.',
       },
-      { learning: "Understanding how to use live updated APIs." },
+      { learning: 'DOM API to query, modify, and create elements.' },
       {
-        learning: "Using animations to make a website more interactive.",
+        learning: 'Using event listeners to add interactivity.',
+      },
+      { learning: 'Using the Web Storage API to persist data.' },
+      {
+        learning: 'Fetch API to get data from a third party.',
+      },
+      {
+        learning: 'Working with canvas and making it work on all devices.',
+      },
+    ],
+  },
+
+  {
+    title: 'Nordstan',
+    href: 'nordstan',
+    subTitle: 'The most used bus in Gothenburg is bus number 16',
+    imgSrc: '/medias/nordstan.webp',
+    videoWebm: '/medias/nordstan.webm',
+    videoMp4: '/medias/nordstan.mp4',
+    github: 'https://github.com/OskarSollenberg/ITTT---ResRobot_Nordstan',
+    netlify: '#',
+    description:
+      'This is a website I created in an early module at Hyper Island. Using an animation I created, the website displays in real-time when bus 16 arrives and leaves Nordstan in Gothenburg. The website is made with HTML, CSS/SASS, and JavaScript.',
+    learnings: [
+      {
+        learning:
+          'Learning JavaScript concepts such as variables, loops, functions.',
+      },
+      { learning: 'Understanding how to use live updated APIs.' },
+      {
+        learning: 'Using animations to make a website more interactive.',
       },
       {
         learning:
-          "Keeping responsiveness with animated elements on the webpage.",
+          'Keeping responsiveness with animated elements on the webpage.',
       },
     ],
   },
   {
-    title: "Hangman",
-    href: "hangman",
-    subTitle: "Creating a classic",
-    imgSrc: "/medias/hangman.webp",
-    videoWebm: "/medias/hangman.webm",
-    videoMp4: "/medias/hangman.mp4",
-    github: "https://github.com/OskarSollenberg/ITTT---Hangman-ver2",
-    netlify: "https://hangmanversion2.netlify.app",
+    title: 'Hangman',
+    href: 'hangman',
+    subTitle: 'Creating a classic',
+    imgSrc: '/medias/hangman.webp',
+    videoWebm: '/medias/hangman.webm',
+    videoMp4: '/medias/hangman.mp4',
+    github: 'https://github.com/OskarSollenberg/ITTT---Hangman-ver2',
+    netlify: 'https://hangmanversion2.netlify.app',
     description:
       "This is a creative take on the classic game, Hangman . The player has 6 tries to guess the word correctly, otherwise they lose. It's done using only vanilla JavaScript, HTML, and CSS. This was my personal project during one of the first modules at Hyper Island.",
     learnings: [
       {
-        learning: "JavaScript concepts such as variables, loops, functions.",
+        learning: 'JavaScript concepts such as variables, loops, functions.',
       },
-      { learning: "How to implement sound in your website." },
+      { learning: 'How to implement sound in your website.' },
       {
         learning:
-          "Learning more about how to create fully responsive websites.",
+          'Learning more about how to create fully responsive websites.',
       },
-      { learning: "HTML, CSS, JS, and SASS." },
+      { learning: 'HTML, CSS, JS, and SASS.' },
     ],
   },
 ];
