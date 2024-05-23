7b5c346c5314a10f73d1ce558793a03a7aea215c
Bwunkaz <122973984+Bwunkaz@users.noreply.github.com>
2024-05-23 14:34:33 +0200

    with orbitcontrols

--- src/components/SceneHome/index.jsx
+++ src/components/SceneHome/index.jsx
@@ -1,17 +1,18 @@
-"use client";
-import { Canvas } from "@react-three/fiber";
-import Model from "./Model";
-import { Environment } from "@react-three/drei";
-import { useEffect } from "react";
+'use client';
+import { Canvas } from '@react-three/fiber';
+import Model from './Model';
+import { Environment } from '@react-three/drei';
+import { useEffect } from 'react';
+import { OrbitControls } from '@react-three/drei';
 
 export default function SceneHome() {
   useEffect(() => {
     const originalStyle = window.getComputedStyle(document.body).cssText;
-    document.body.style.cssText = "position: fixed; top: -${window.scrollY}px";
+    document.body.style.cssText = 'position: fixed; top: -${window.scrollY}px';
 
     return () => {
       document.body.style.cssText = originalStyle;
-      window.scrollTo(0, parseInt(document.body.style.top || "0") * -1);
+      window.scrollTo(0, parseInt(document.body.style.top || '0') * -1);
     };
   }, []);
 
@@ -19,8 +20,9 @@ export default function SceneHome() {
     <div className={`w-[100dvw] h-[100dvh] -z-10`}>
       <Canvas>
         <Model />
+        <OrbitControls enableZoom={false} />
         <directionalLight />
-        <Environment preset="city" />
+        <Environment preset='night' background />
       </Canvas>
     </div>
   );
