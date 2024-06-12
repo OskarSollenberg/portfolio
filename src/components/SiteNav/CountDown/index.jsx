546b1d5b3e7353d3dca20b74a49cc9b846c8c151
Bwunkaz <122973984+Bwunkaz@users.noreply.github.com>
2024-06-12 14:49:16 +0200

    updated countdown date

--- src/components/SiteNav/CountDown/index.jsx
+++ src/components/SiteNav/CountDown/index.jsx
@@ -2,7 +2,7 @@ import { AnimatePresence, motion } from 'framer-motion';
 import { useEffect, useRef, useState } from 'react';
 
 // NOTE: Change this date to whatever date you want to countdown to :)
-const COUNTDOWN_FROM = '2024-09-01';
+const COUNTDOWN_FROM = '2024-09-23';
 
 const SECOND = 1000;
 const MINUTE = SECOND * 60;
