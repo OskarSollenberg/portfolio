fe0e1a51613d095c8eb8f482eea730812315b7ee
Bwunkaz <122973984+Bwunkaz@users.noreply.github.com>
2024-06-19 17:54:24 +0200

    .

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -6,18 +6,17 @@ import ProgressBar from '@/src/components/ProgressBar';
 
 export default function About() {
   return (
-    <div className='h-[200vh]'>
-      <div className='absolute bottom-0 z-[10000]'>
-        <ProgressBar />
-      </div>
-      <div className='absolute inset-0 flex items-center justify-center text-[5rem] sm:text-[6rem] md:text-[7rem] lg:text-[8rem] text-[#F3691F] translate-y-[7rem] sm:translate-y-[8rem] md:translate-y-[9rem] lg:translate-y-[10rem] xl:translate-y-[11rem] text-center '>
-        <Arrow />
-      </div>
-      <div className='translate z-50 h-screen'>
-        <SceneAbout />
-      </div>
-      <main className='absolute w-screen h-screen text-black'>
-        {/* <div className='h-screen lg:hidden'></div>
+    <>
+      <ProgressBar />
+      <div className='h-[300vh]'>
+        <div className='absolute inset-0 flex items-center justify-center text-[5rem] sm:text-[6rem] md:text-[7rem] lg:text-[8rem] text-[#F3691F] translate-y-[7rem] sm:translate-y-[8rem] md:translate-y-[9rem] lg:translate-y-[10rem] xl:translate-y-[11rem] text-center '>
+          <Arrow />
+        </div>
+        <div className='translate z-50 h-screen'>
+          <SceneAbout />
+        </div>
+        <main className='absolute w-screen h-screen text-black'>
+          {/* <div className='h-screen lg:hidden'></div>
         <div className='relative w-screen h-screen hidden lg:block mb-[10rem]'>
           <Image
             className='w-screen h-screen'
@@ -30,18 +29,19 @@ export default function About() {
             priority={true}
           />
         </div> */}
-        {/* <div className=' w-screen'></div> */}
-        <div className='w-[90vw] lg:w-[80vw] h-auto mx-auto rounded-lg overflow-hidden  mb-[5rem]'>
-          <Image
-            quality={60}
-            width={1920}
-            height={1080}
-            src='/medias/studio.webp'
-          />
-        </div>
-        <AboutText />
-        <div className='w-screen h-[20rem]'></div>
-      </main>
-    </div>
+          {/* <div className=' w-screen'></div> */}
+          <div className='w-[90vw] lg:w-[80vw] h-auto mx-auto rounded-lg overflow-hidden  mb-[5rem]'>
+            <Image
+              quality={60}
+              width={1920}
+              height={1080}
+              src='/medias/studio.webp'
+            />
+          </div>
+          <AboutText />
+          <div className='w-screen h-[20rem]'></div>
+        </main>
+      </div>
+    </>
   );
 }
